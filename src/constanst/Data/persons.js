516eff9186e51f4575406e3b97d7713a58b1afb6
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2023-02-11 17:48:46 -0300

    add data for simulator DB for read data on APP

---
+++ src/constanst/Data/persons.js
@@ -0,0 +1,19 @@
+export const persons =[
+  {
+    id: 1,
+    name: 'Susana Pinto Corvalan',
+    mail: 'susanapintoc@gmail.com',
+    mail2: 'kulinilla@gmail.com',
+    phone: '+569 8224 4955',
+    phone2: '+56 65 2224 5006',
+    facebook: 'https://www.facebook.com/SusyPinto',
+    intagram: 'https://www.instagram.com/susypinto/',
+    linkedin: 'https://www.linkedin.com/in/susana-pinto-corvalan-5b1b3b1a/',
+    address: 'Jose Miguel Balmaceda 15',
+    city: 'Puerto Montt',
+    country: 'Chile',
+    latitude: '41284.4S',
+    longitude: '725646.9W',
+    image: 'https://scontent.fpmc1-1.fna.fbcdn.net/v/t31.18172-8/15775058_10210692317362081_124673145645881365_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHl6Z6vgFnvI_SQa48SpMCoWOYdYaYUIVpY5h1hphQhWjCrc89lUE1YecvSy2mHojQ&_nc_ohc=RuKeZqocs-0AX-I_s0_&_nc_ht=scontent.fpmc1-1.fna&oh=00_AfDEtfGYkZcRDModfllFuQIYn4YcoEKXVq2ywG6UT8Wg4g&oe=640F52DF'
+  }
+]
---
+++ src/constanst/Data/task.js
@@ -0,0 +1,9 @@
+export const tasks = [
+  {
+    name: 'Armar PC',
+    description: 'Armar una PC para jugar',
+    status: 'Pendiente',
+    reason: 'Urgente',
+    date: '2021-10-10'
+  }
+]
