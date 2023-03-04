893d0031137655895c608ba97a6737d2931dff92
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2023-03-04 10:10:19 -0300

    resolve the bug on camare

--- src/components/imagenSelector/style.js
+++ src/components/imagenSelector/style.js
@@ -3,14 +3,19 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    marginHorizontal: 20,
     alignItems: "center",
     justifyContent: "center",
+    width: '100%',
+    height: '67%',
+    backgroundColor: Color.white,
   },
 
   imageContainer: {
-    width:'100%',
+    width:'80%',
     height: 200,
     marginBottom: 10,
+    marginTop:20,
     justifyContent: 'center',
     alignItems: 'center',
     borderBottomColor: Color.primary,
